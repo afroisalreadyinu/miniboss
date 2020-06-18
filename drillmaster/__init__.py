75384c9041051dbfc8f8cf41a17bd6d84b77e141
Ulas Tuerkmen <ulas.tuerkmen@gmail.com>
2020-06-18 20:48:58 +0200

    Fixed two silly mistakes

--- drillmaster/__init__.py
+++ drillmaster/__init__.py
@@ -1,2 +1,3 @@
 from .cli import cli
 from .services import Service
+from .context import Context
--- drillmaster/docker_client.py
+++ drillmaster/docker_client.py
@@ -4,6 +4,6 @@ _the_docker = None
 
 def get_client():
     global _the_docker
-    if _the_docker is not None:
+    if _the_docker is None:
         _the_docker = docker.from_env()
     return _the_docker
