0174df1dd70985d11cc17beb115408493ce6616b
Ulas Tuerkmen <ulas.tuerkmen@gmail.com>
2020-08-03 22:38:38 +0200

    Added save_to and load_from to context

--- miniboss/context.py
+++ miniboss/context.py
@@ -1,6 +1,20 @@
+import json
+import pathlib
+
 from miniboss.exceptions import ContextError
 
 class _Context(dict):
+
+    def save_to(self, directory):
+        path = pathlib.Path(directory) / ".miniboss-context"
+        with open(path, 'w') as context_file:
+            context_file.write(json.dumps(self))
+
+    def load_from(self, directory):
+        path = pathlib.Path(directory) / ".miniboss-context"
+        with open(path, 'r') as context_file:
+            new_data = json.load(context_file)
+        self.update(**new_data)
 
     def extrapolate(self, env_value):
         if not hasattr(env_value, "format"):
--- tests/test_context.py
+++ tests/test_context.py
@@ -1,4 +1,7 @@
+import os
+import json
 import unittest
+import tempfile
 
 import pytest
 
@@ -41,3 +44,17 @@ class ContextTests(unittest.TestCase):
         assert output == {'key1': 'This is 123',
                           'key2': 'And this is hello',
                           'key3': 456}
+
+    def test_save_to_load_from(self):
+        directory = tempfile.mkdtemp()
+        context = _Context(blah=123, yada="hello")
+        context.save_to(directory)
+        path = os.path.join(directory, ".miniboss-context")
+        assert os.path.exists(path)
+        with open(path, 'r') as in_file:
+            data = json.load(in_file)
+        assert data == {'blah': 123, 'yada': 'hello'}
+        new_context = _Context()
+        new_context.load_from(directory)
+        assert new_context['blah'] == 123
+        assert new_context['yada'] == "hello"
